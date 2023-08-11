14d5f193b4e5e83b4413a92d4aefbf2e5046c3a1
cily-codes <okoroamarachicecilia@gmail.com>
2023-08-11 16:40:22 +0000

    update

--- 0x02-python-import_modules/0-add.py
+++ 0x02-python-import_modules/0-add.py
@@ -1,13 +1,13 @@
 #!/usr/bin/python3
+from add_0 import add
+
 a = 1
 b = 2
 
 if __name__ == "__main__":
-    import add_0
 
     """
     Prints the result of the addition of two numbers
     """
 
-    result = add_0.add(a, b)
-    print("{} + {} = {}".format(a, b, result))
+    print("{:d} + {:d} = {:d}".format(a, b, add(a, b)))
