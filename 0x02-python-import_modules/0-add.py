797e3e04f43c77070889f3ef14f21d9cfecdf3c0
cily-codes <okoroamarachicecilia@gmail.com>
2023-08-11 16:36:54 +0000

    import a function and print results

---
+++ 0x02-python-import_modules/0-add.py
@@ -0,0 +1,13 @@
+#!/usr/bin/python3
+a = 1
+b = 2
+
+if __name__ == "__main__":
+    import add_0
+
+    """
+    Prints the result of the addition of two numbers
+    """
+
+    result = add_0.add(a, b)
+    print("{} + {} = {}".format(a, b, result))
