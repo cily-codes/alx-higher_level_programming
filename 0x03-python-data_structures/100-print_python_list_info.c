d78805e63bb52f0bcf530d45583ca3c6b8ea1017
cily-codes <okoroamarachicecilia@gmail.com>
2023-08-15 23:57:58 +0000

    fixing the advanced task

--- 0x03-python-data_structures/100-print_python_list_info.c
+++ 0x03-python-data_structures/100-print_python_list_info.c
@@ -1,7 +1,5 @@
 #include <stdio.h>
-#include "/usr/include/python3.4/Python.h"
-#include "/usr/include/python3.4/listobject.h"
-#include "/usr/include/python3.4/object.h"
+#include <Python.h>
 
 /**
  * print_python_list_info - prints basic info about Python lists
