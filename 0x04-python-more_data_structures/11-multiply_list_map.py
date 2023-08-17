196cc05b485e153e4edd64316d8a2940c2125d29
cily-codes <okoroamarachicecilia@gmail.com>
2023-08-17 00:28:37 +0000

    updating task 11

--- 0x04-python-more_data_structures/11-multiply_list_map.py
+++ 0x04-python-more_data_structures/11-multiply_list_map.py
@@ -1,5 +1,5 @@
 #!/usr/bin/python3
-def mutiply_list_map(my_list=[], number=0):
+def multiply_list_map(my_list=[], number=0):
     """
     returns a list with all values multiplied by a number without using any loops
     """
